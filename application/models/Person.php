43f6626c9588ffbc432ae9add284554bddb5f1c8
Simon Hodel <simonhodel@gmx.ch>
2013-04-23 15:19:01 +0200

    add model Person
    Signed-off-by: Thomas Bernhart <thomascbernhart@gmail.com>

---
+++ application/models/Person.php
@@ -0,0 +1,66 @@
+﻿<?php
+
+class Person{
+	
+	public $firstname = string;
+	public $lastname = string;
+	public $title = string;
+	// public $email = string;
+	public $gender = bool; // m <=> true; w <=> false
+	
+	// Constructor
+	function __construct($pFirstname, $pLastname, $pTitle = "", /* $pEmail = "",$*/ $pGender = true){
+		$this->firstname = $pFirstname;
+		$this->lastname = $pLastname;
+		$this->title = $pTitle;
+		// $this->email = $pEmail;
+		$this->gender = $pGender;
+	}
+	
+	// Setters
+	
+	public function setFirstname($pFirstname){
+		$this->firstname = $pFirstname;
+	}
+	
+	public function setLastname($pLastname){
+		$this->lastname = $pLastname;
+	}
+	
+	public function setTitle($pTitle){
+		$this->title = $pTitle;
+	}
+	
+	public function setGender($pGender){
+		$this->gender = $pGender;
+	}
+	
+	// Getters
+	
+	public function getFirstname(){
+		return $this->firstname;
+	}
+	
+	public function getLastname(){
+		return $this->lastname;
+	}
+	
+	public function getTitle(){
+		return $this->title;
+	}
+	
+	public function getGender(){
+		return $this->gender;
+	}
+	
+	// Other functions
+	
+	public function iterateVisible(){
+		echo "Person::iterateVisible:\n";
+		foreach($this as $key => $value){
+			print "$key => $value\n";
+		}
+	}
+}
+
+?>
