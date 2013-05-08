661ef382764da33537ca7ea3a35dda35f61f26b7
Thomas Bernhart <thomascbernhart@gmail.com>
2013-05-08 10:42:47 +0200

    Add migration to add users table

---
+++ application/migrations/002_Create_users_table.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Migration_Create_users_table extends CI_Migration {
+
+	public function up() {		
+		/* create table documents */
+		$this->dbforge->add_field('id');
+		// $this->dbforge->add_key('id', true);
+		$docFields = array(
+				'aaiId' => array('type' => 'VARCHAR', 'constraint' => '100'),
+				'username' => array('type' => 'VARCHAR', 'constraint' => '100'),		
+				'firstName' => array('type' => 'VARCHAR', 'constraint' => '100'),
+				'lastName' => array('type' => 'VARCHAR', 'constraint' => '100'),
+				'lastLogin' => array('type' => 'TIMESTAMP'),	
+				'created' => array('type' => 'TIMESTAMP'),
+			);
+		$this->dbforge->add_field($docFields);
+		$this->dbforge->create_table('users', true);
+	
+		/* get tablenames with prefixes */
+		$usersTableWPrfx = $this->db->dbprefix('users'); // get the tableName with prefix
+
+		/* change engines to InnoDB and add constraints */
+		$this->db->query("ALTER TABLE `$usersTableWPrfx` ENGINE=InnoDB, ADD CONSTRAINT UNIQUE (`username`, `aaiId`)");
+	}
+
+	public function down() {
+		$this->dbforge->drop_table('users');
+	}	
+}
+
