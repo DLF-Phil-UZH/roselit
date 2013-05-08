b033d16d843b1cff5dd8f5d1cc9e770038172a7c
Thomas Bernhart <thomascbernhart@gmail.com>
2013-05-08 11:28:45 +0200

    Add User_model
    Add User_model as subclass of Person_model
    User_model provides additional attributes: username, aaiId and role
    getters/setters for these attributes

---
+++ application/models/User_model.php
@@ -0,0 +1,60 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class User_model extends Person_model {
+
+	private $_username;
+    private $_aaiId;
+    private $_role = 'user';
+
+    public function __construct($pUsername, $pAaiId, $pFirstname, $pLastname, $pTitle = "", /* $pEmail = "",$*/ $pGender = true) {
+		parent::__construct($pFirstname, $pLastname, $pTitle, /* $pEmail = "",$*/ $pGender);
+		$this->_username = $pUsername;
+		$this->_aaiId = $pAaiId;
+    }
+
+    /* getters: */
+
+    public function getAaiId() {
+        return $this->_aaiId;
+    }
+
+    public function getRole() {
+        return $this->_role;
+    }
+
+    public function isAdmin() {
+        return ($this->_role == 'admin');
+    }
+
+
+    /* setters: */
+
+	public function setUsername($pUsername) {
+		if ( is_string($username) ) { // TODO: Länge des Strings prüfen!
+            $this->_username = $pUsername;
+        } else {
+            throw new InvalidArgumentException('string expected');
+        }
+        return $this;
+
+	}
+
+    public function setAaiId( $pAaiId ) {
+        if ( is_string($pAaiId) ) {
+            $this->_aaiId = $pAaiId;
+        } else {
+            throw new InvalidArgumentException('string expected');
+        }
+        return $this;
+    }
+
+    public function setRole($pRole) {
+        if ( is_string($pRole) ) {
+            $this->_role = $pRole;
+        } else {
+            throw new InvalidArgumentException('string expected');
+        }
+        return $this;
+    }
+}
+
