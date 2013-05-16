a1306a617351510aeffc9d00e0d28f51292124fa
Simon Hodel <simonhodel@gmx.ch>
2013-05-16 03:37:44 +0200

    Created first beta version document list controller and corresponding view script, partially hard coded for testing

---
+++ application/controllers/document_list.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+require_once("document_model.php");
+
+// Based on a CI-Tutorial: http://ellislab.com/codeigniter/user-guide/tutorial/news_section.html
+class Document_list extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('document_list_mapper');
+	}
+
+	public function index()
+	{
+		$data['documentList'] = $this->Document_list_mapper->get(1); // Temporarily hard coded for testing
+		$data['title'] = "Liste 1"; // Temporarily hard coded for testing
+		
+		$this->load->view('list/index', $data);
+	}
+	
+}
---
+++ application/views/document_list/index.php
@@ -0,0 +1,29 @@
+﻿<?php // Based on a CI-Tutorial: http://ellislab.com/codeigniter/user-guide/tutorial/news_section.html ?>
+
+<h2><?php echo $list["title"]?></h2>
+<p>Ersteller: <?php echo $list["creator"]?></p>
+<p>Administrator: <?php echo $list["admin"]?></p>
+<p>Erstellt am: <?php echo $list["created"]?></p>
+<p>Zuletzt ge&auml;ndert am: <?php echo $list["lastUpdated"]?></p>
+<h3>Dokumente:</h3>
+
+<?php foreach($list["documentIds"] as $lDocument): // Will not work, copied from tutorial
+
+// TODO: Generate Document object for every loop iteration?>
+
+<p>
+	<a target="_blank" href="<?php $lDocument->getFileName() ?>"><?php echo 
+		$lDocument->getAuthors() . " (" .
+		$lDocument->getYear() . "), &ldquo;" .
+		$lDocument->getTitle() . "&rdquo;, in: " .
+		$lDocument->getEditors() . ": <i>" .
+		$lDocument->getPublication() . "</i>, " .
+		$lDocument->getPublishingHouseAndPlace() . " " .
+		$lDocument->getPages() . ". (" .
+		"pdf" /* Temporarily hard coded for testing, TODO: replace with substring function on getFileName() */ . ", "
+		"3.3 MB" /* TODO: replace with function that calculates file size */ . ")"
+		?>
+	</a>
+</p>
+
+<?php endforeach ?>
