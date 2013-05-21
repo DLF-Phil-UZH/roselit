8865529c008f8ce79dc298400571a70595685764
Thomas Bernhart <thomascbernhart@gmail.com>
2013-05-21 12:27:50 +0200

    Adjust columnnames in Crud_service, add function getUsersCrud()

--- application/libraries/crud_service.php
+++ application/libraries/crud_service.php
@@ -19,10 +19,10 @@ class Crud_service {
 		try{
 			$crud->set_table('documents');
 			$crud->set_subject('Dokument');
-			$crud->set_relation('creator','users','{username} - {firstName} {lastName}');
-			$crud->set_relation('admin','users','{username} - {firstName} {lastName}');		
+			$crud->set_relation('creator','users','{username} - {firstname} {lastName}');
+			$crud->set_relation('admin','users','{username} - {firstname} {lastname}');		
 			$crud->set_relation_n_n('Listen', 'documents_documentLists', 'documentLists', 'documentId', 'documentListId', 'title');
-			$crud->columns('explicitId', 'title', 'authors', 'publication', 'year', 'place', 'startPage', 'endPage', 'fileName');
+			$crud->columns('explicitId', 'title', 'authors', 'publication', 'publishingHouseAndPlace', 'year', 'pages', 'fileName');
 
 			// customize fields:
 			//$crud->callback_field('lastUpdated',array($this,'_make_field_lastUpdated_readonly'));	
@@ -39,11 +39,9 @@ class Crud_service {
 				 ->display_as('publication', 'Werk- oder Zeitschriftentitel (mit Vol.)')
 				 ->display_as('editors', 'Herausgeber (mit : und (edd.))')
 				 ->display_as('year', 'Jahr')
-				 ->display_as('place', 'Ort')
-				 ->display_as('startPage', 'von (Seite)')
-				 ->display_as('endPage', 'bis (Seite)')
+				 ->display_as('publishingHouseAndPlace', 'Ort und Verlag')
+				 ->display_as('pages', 'Seiten')
 				 ->display_as('fileName', 'Datei')
-				 ->display_as('publisher', 'Verlag')
 				 ->display_as('creator', 'erstellt von')
 				 ->display_as('admin', 'verwaltet von');
 		
@@ -98,7 +96,7 @@ class Crud_service {
 		try{
 			$crud->set_table('users');
 			$crud->set_subject('Benutzer');
-			$crud->columns('id', 'aaiId', 'username', 'firstName', 'lastName');
+			$crud->columns('id', 'aaiId', 'username', 'firstname', 'lastname');
 
 			// edit fields:
 			$crud->field_type('created', 'readonly')
@@ -109,8 +107,8 @@ class Crud_service {
 			$crud->display_as('id','ID')
 				  ->display_as('aaiId', 'AAI UniqueID')
 				  ->display_as('username', 'Benutzername')
-				  ->display_as('firstName', 'Vorname')
-				  ->display_as('lastName', 'Nachname')
+				  ->display_as('firstname', 'Vorname')
+				  ->display_as('lastname', 'Nachname')
 				  ->display_as('lastLogin', 'letzter Login am')
 				  ->display_as('created', 'registriert seit');
 
@@ -120,6 +118,40 @@ class Crud_service {
 		}
 		return $output;
 	}
+
+	public function getUserRequestsCrud() {
+		$crud = $this->_getCrud();
+		$output = '';
+
+		/* config */
+		try{
+			$crud->set_table('user_requests');
+			$crud->set_subject('Zugriffsanfragen');
+			$crud->columns('aaiId', 'firstname', 'lastname', 'email', 'created');
+
+			// Field aliases:
+			$crud->display_as('aaiId', 'AAI UniqueID')
+				  ->display_as('username', 'Benutzername')
+				  ->display_as('firstname', 'Vorname')
+				  ->display_as('lastname', 'Nachname')
+				  ->display_as('created', 'eingegangen am');
+
+			// edit fields:
+			$crud->field_type('created', 'readonly');
+			
+			$crud->unset_add()
+					->unset_edit();
+
+			// TODO: add custom action to accept request
+			$crud->add_action('Accept', '', 'admin/user_requests/accept','ui-icon-plus');
+
+			
+			$output = $crud->render();
+		}catch(Exception $e){
+			show_error($e->getMessage().' --- '.$e->getTraceAsString());
+		}
+		return $output;
+	}
 }
 
-/* End of file CrudService.php */
+/* End of file crud_service.php */
