e5393693dbe438351dcb59bcb33f11ccaab58302
Simon Hodel <simonhodel@gmx.ch>
2013-05-21 11:20:24 +0200

    Finished first version of HTML output, according to former HTML output in excel file (still missing: representation of edition and file size, as well as correct hyperlinks)

--- application/controllers/document_list.php
+++ application/controllers/document_list.php
@@ -1,19 +1,15 @@
 ﻿<?php
 
-require_once("document_model.php");
-
 // Based on a CI-Tutorial: http://ellislab.com/codeigniter/user-guide/tutorial/news_section.html
 class Document_list extends CI_Controller {
 	
-	public function __construct()
-	{
+	public function __construct(){
 		parent::__construct();
 		$this->load->model('document_list_mapper');
 	}
 
-	public function index()
-	{
-		$data['documentList'] = $this->Document_list_mapper->get(1); // Temporarily hard coded for testing
+	public function index(){
+		$data['documentList'] = $this->document_list_mapper->get(1); // Temporarily hard coded for testing
 		$this->load->view('document_list/index', $data);
 	}
 	
--- application/views/document_list/index.php
+++ application/views/document_list/index.php
@@ -3,25 +3,32 @@
 <h2><?php echo $documentList->getTitle(); ?></h2>
 <p>Ersteller: <?php echo $documentList->getCreator(); ?></p>
 <p>Administrator: <?php echo $documentList->getAdmin(); ?></p>
-<p>Erstellt am: <?php echo $documentList->created(); ?></p>
-<p>Zuletzt ge&auml;ndert am: <?php echo $documentList->lastUpdated(); ?></p>
+<p>Erstellt am: <?php
+	$lDatum = date_format($documentList->getCreated(), "d.m.Y, H:i:s");
+	$lDatum = $lDatum." Uhr";
+	echo $lDatum;
+?></p>
+<p>Zuletzt ge&auml;ndert am: <?php
+	$lDatum = date_format($documentList->getLastUpdated(), "d.m.Y, H:i:s");
+	$lDatum = $lDatum." Uhr";
+	echo $lDatum;
+?></p>
 <h3>Dokumente:</h3>
 
-<?php foreach($documentList->getDocuments() as $lDocument): // Will not work, copied from tutorial
-
-// TODO: Generate Document object for every loop iteration?>
+<?php foreach($documentList->getDocuments() as $lDocument): ?>
 
 <p>
 	<a target="_blank" href="<?php $lDocument->getFileName() ?>"><?php echo 
 		$lDocument->getAuthors() . " (" .
+		// TODO: add edition to document model (wrong data in database)
 		$lDocument->getYear() . "), &ldquo;" .
 		$lDocument->getTitle() . "&rdquo;, in: " .
-		$lDocument->getEditors() . ": <i>" .
+		(strlen($lDocument->getEditors()) ? $lDocument->getEditors() . " <i>" : "<i>") .
 		$lDocument->getPublication() . "</i>, " .
-		$lDocument->getPublishingHouseAndPlace() . " " .
+		(strlen($lDocument->getPublishingHouseAndPlace()) ? $lDocument->getPublishingHouseAndPlace() . " " : "") .
 		$lDocument->getPages() . ". (" .
-		"pdf" /* Temporarily hard coded for testing, TODO: replace with substring function on getFileName() */ . ", "
-		"3.3 MB" /* TODO: replace with function that calculates file size */ . ")"
+		substr(strrchr($lDocument->getFileName(), "."), 1) . ", " .
+		"3.3 MB" /* TODO: replace with function that calculates file size */ . ")";
 		?>
 	</a>
 </p>
