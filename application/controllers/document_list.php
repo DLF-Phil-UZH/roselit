466d7882ed64ec67f206cb7e7dbf4babc43dec4c
Thomas Bernhart <thomascbernhart@gmail.com>
2013-08-12 14:51:48 +0200

    Add handling if a list doesn't exist.

--- application/controllers/document_list.php
+++ application/controllers/document_list.php
@@ -12,6 +12,9 @@ class Document_list extends CI_Controller {
 	public function view($pId){
 		$this->output->set_header("Content-Type: text/html; charset=utf-8");
 		$lDocumentList = $this->document_list_mapper->get($pId);
+        if($lDocumentList === false) {
+            show_404(current_url());
+        }
 		if($lDocumentList->getPublished()){
 			$this->load->view('document_list/index', array("documentList" => $lDocumentList));
 		}
