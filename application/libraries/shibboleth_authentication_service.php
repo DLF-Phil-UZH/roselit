2bd706c40fdebd5b1d5cb1eadf3aca0086bc8b42
Thomas Bernhart <thomascbernhart@gmail.com>
2013-08-16 01:31:08 +0200

    Add function verify_user_access_request, some small changes

--- application/libraries/shibboleth_authentication_service.php
+++ application/libraries/shibboleth_authentication_service.php
@@ -2,10 +2,14 @@
 
 class Shibboleth_authentication_service {
 
+    private $session;
+    private $shibboleth_session_exists = false;
+
 	public function __construct() {
 		// TODO: load settings from config file
-		$ci = &get_instance();
-		$ci->load->database();
+        if (isset($_SERVER['Shib-Identity-Provider'])) {
+			$this->shibboleth_session_exists = true;
+        }
 	}
 	
 	/**
@@ -16,11 +20,7 @@ class Shibboleth_authentication_service {
 	 *
 	 */
 	public function verify_shibboleth_session() {
-		// TODO: check if shibboleth sesssion exists
-		if (isset($_SERVER['Shib-Identity-Provider'])) {
-			return true;
-		}
-		return false;
+        return $this->$shibboleth_session_exists;
 	}
 
 	/**
@@ -34,23 +34,41 @@ class Shibboleth_authentication_service {
 	 * @return	user_model/boolean	user_model on success, false else
 	 * @access	public
 	 */
-	public function verify_user() {
-		if ($this->verify_shibboleth_session() !== false) {
-			if (!isset($_SERVER['Shib-SwissEP-UniqueID'])) {
-				throw new Exception('Shib-SwissEP-UniqueID not set.');
-			}
-			$lAaiId = $_SERVER['Shib-SwissEP-UniqueID'];
+    public function verify_user() {
+        if ($this->verify_shibboleth_session() !== false) {
+
+            if (!isset($_SERVER['Shib-SwissEP-UniqueID'])) {
+                throw new Exception('Shib-SwissEP-UniqueID not set.');
+            }
+            $lAaiId = $_SERVER['Shib-SwissEP-UniqueID'];
 			// check if a user with that shibboleth id exists in the db
 			// if not create one, but set role to "not granted"
 			$ci = &get_instance();
-			$ci->load->model('user_mapper');
-			$user = $ci->user_mapper->getByAaiId($lAaiId);
-			if ($user !== false) {
-				return $user;
-			}	
-		}
-		return false;
+            $lAaiId = $_SERVER['Shib-SwissEP-UniqueID'];
+            $ci->load->model('user_mapper');
+            $user = $ci->user_mapper->getByAaiId($lAaiId);
+            if ($user !== false) {
+                return $user;
+            }
+        }
+        return false;
 	}
+
+    public function verify_user_access_request() {
+        if ($this->verify_shibboleth_session() !== false) {
+            if (!isset($_SERVER['Shib-SwissEP-UniqueID'])) {
+                throw new Exception('Shib-SwissEP-UniqueID not set.');
+            }
+            $lAaiId = $_SERVER['Shib-SwissEP-UniqueID'];
+            $ci = &get_instance();
+            $ci->load->database();
+            $lQuery = $ci->db->get_where('user_requests', array("aaiId" => $lAaiId), 1);
+            if ($lQuery->num_rows() > 0) {
+			    return $lQuery->row();
+            }
+        }
+        return false;
+    }
 
 }
 
